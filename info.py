13d5fe2b6e0ba81f5e1d5d9e31d765fee7f142e3
RaizFF <rahulyoutubar@gmail.com>
2025-09-19 18:27:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://earn4link.in')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'earn4link.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '37e6905cda78e16ad51757112b77084c6ebfd4c1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TamilMoviesHD_X/28') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
@@ -197,6 +197,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
