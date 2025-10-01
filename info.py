3fc573328a58fdc0dddf53755919c5f9bb5ab840
RaizFF <rahulyoutubar@gmail.com>
2025-10-01 08:53:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,8 +60,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "MongoDb url & Db name
-mongodb+srv://rahulyoutubar_db:OuakuqUodiEW2mNA@cluster0.gyjekgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-")
+mongodb+srv://rahulyoutubar_db:OuakuqUodiEW2mNA@cluster0.gyjekgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
@@ -199,6 +198,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
